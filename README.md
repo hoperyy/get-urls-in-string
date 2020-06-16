1c3792f52a1b5da0e8d1df2d2ec8a569a441ad40
hoperyy <antfootmail@gmail.com>
2020-06-16 14:12:57 +0800

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+## 项目名称
+
+## 项目介绍
+
+## 项目人员
+
+## 目录结构
+
+```
+pages/  （入口，必须命名为 pages）
+	index/   首页（页面名称，可修改）
+		modules/ 各个模块
+		routes/ 子页面
+		index.html
+		index.js（入口文件，且必须命名为 index.js）
+		index.less
+```
---
+++ qute.config.js
@@ -0,0 +1,39 @@
+/*
+ * @vbuilder.config.js
+ */
+
+// vbuilder 文档：http://vbuilder-doc.daily.vdian.net/vbuilder/doc/default-vue-scaffold-config.html
+
+module.exports = ({ userFolder, srcFolder, buildFolder, currentEnv, webpack, webpackDevServer }) => {
+    const path = require('path');
+    const fse = require('fs-extra');
+
+    return {
+        buildFolder: './dist',
+        noPolyfill: true,
+
+        commonJs: false,
+
+        // 可以合并到 webpack 的配置，按照 webpack 的配置风格
+        webpackConfig: {
+            output: {
+                library: 'getUrlsInString',
+                libraryTarget: 'umd'
+            }
+        },
+
+        afterBuild() {
+            const buildFolder = path.join(userFolder, 'dist');
+            const from = path.join(buildFolder, 'static/index/index.js');
+
+            // 要发布的目标文件
+            const to = path.join(buildFolder, 'index.js');
+
+            fse.copySync(from, to);
+            fse.removeSync(path.join(buildFolder, 'pages'));
+            fse.removeSync(path.join(buildFolder, 'static'));
+        }
+    };
+};
+
+module.exports.vbuilderScaffold = '@vdian/mechanic';
